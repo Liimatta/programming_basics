c946d1410a3bc2646881eecda19ff8baf5631c02
Janne Liimatta <janneliimatta@hotmail.com>
2017-09-21 15:48:20 +0300

    task 1 almost done

---
+++ conditional-statements/Conditional-statements/Conditional-statements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero (evaluatedNumber) on negatiivinen");
+            }
+            else if (evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero (evaluatedNumber) on positiivinen");
+            }
+            
+            Console.WriteLine($"Syötetty arvo {userInput}");
+            Console.WriteLine("Syötit {0}",userInput);
+            Console.ReadKey();
+            
+        }
+    }
+}
