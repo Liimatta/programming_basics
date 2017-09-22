894669d851003f384de6e29bdffe630b414e2888
Janne Liimatta <janneliimatta@hotmail.com>
2017-09-22 09:12:41 +0300

    Conditional_statements_t2

--- conditional-statements/Conditional_statements_t2/Conditional_statements_t2/Program.cs
+++ conditional-statements/Conditional_statements_t2/Conditional_statements_t2/Program.cs
@@ -10,6 +10,26 @@ namespace Conditional_statements_t2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kirjoita luku. Ohjelma kertoo onko se pariton vai parillinen");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.Write("Numero on parillinen");
+                Console.Read();
+            }
+            
+            else
+            {
+                Console.Write("Numero on pariton");
+                Console.Read();
+            }
+
         }
     }
 }
