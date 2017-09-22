794daba0e1a84c52b42a28b1e6645a596ad4674e
Janne Liimatta <janneliimatta@hotmail.com>
2017-09-22 09:44:33 +0300

    Conditional_statements_t3

---
+++ conditional-statements/Conditional_statements_t3/Conditional_statements_t3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_statements_t3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä luku. Ohjelma kertoo onko se positiivinen vai negatiivinen, sekä onko luku pariton vai parillinen");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber < 0 && evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja parillinen");
+            }
+            else if (evaluatedNumber > 0 && evaluatedNumber % 2 ==0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen ja parillinen");
+            }
+            else if (evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen ja pariton");
+            }
+            else if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja pariton");
+            }
+            else if (evaluatedNumber == 0)
+            {
+                Console.WriteLine($"Numero 0 on 0");
+            }
+
+            Console.WriteLine($"Syötetty arvo {userInput}");
+            Console.ReadKey();
+        }
+    }
+}
