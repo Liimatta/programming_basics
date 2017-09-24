4b8e83a286ef750063e8467893bd46225ad788bd
Janne Liimatta <janneliimatta@hotmail.com>
2017-09-24 16:05:08 +0300

    t4 doned

--- conditional-statements/Conditional_statements_t4/Conditional_statements_t4v2/Conditional_statements_t4v2/Program.cs
+++ conditional-statements/Conditional_statements_t4/Conditional_statements_t4v2/Conditional_statements_t4v2/Program.cs
@@ -32,22 +32,28 @@ namespace Conditional_statements_t4v2
             int c;
             int.TryParse(userInput3, out c);
 
-                if (a > b && b < c) ;
+            if (a > b && b < c)
+            
                 Console.WriteLine($"{b} {c} {a}");
-
-                else.if (a > b && c < b) ;
+            
+            else if (a > b && c < b)
+            
                 Console.WriteLine($"{c} {b} {a}");
+            
 
-                else.if (b > a && c < a) ;
+            else if (b > a && c < a)
+            
                 Console.WriteLine($"{b} {a} {c}");
-
-                else.if (b > a && a < c) ;
+            
+            else if (b > a && a < c)
+            
                 Console.WriteLine($"{b} {c} {a}");
-
-                else.if (c > b && b > a) ;
-                Console.WriteLine($"{c} {b} {a}");}
-
-                else.if (c > a && a > b) ;
+            
+            else if (c > b && b > a)
+            
+                Console.WriteLine($"{c} {b} {a}");
+            
+            else if (c > a && a > b)
                 Console.WriteLine($"{c} {a} {b}");
             
             Console.ReadKey();
