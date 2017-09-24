3955e6546c535342f093cd30d2740fd412d1a10b
Janne Liimatta <janneliimatta@hotmail.com>
2017-09-24 15:51:25 +0300

    t4

---
+++ conditional-statements/Conditional_statements_t4/Conditional_statements_t4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_statements_t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[10];
+            int i, j, tmp;
+            Console.WriteLine("Kirjoita kolme numeroa:");
+            
+            
+
+                for (i = 0; i < 3; i++)
+                {
+                Console.Write(
+                    "Numero {0} : ",i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            for (i=0; i<3; i++)
+            {
+                for (j=i+1; j<3; j++)
+                {
+                    if (arr1[j] < arr1[i])
+                    {
+                        tmp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = tmp;
+                    }
+                }
+                Console.Write("Nousevassa järjestyksessä");
+                for (i=0; i<3; i++)
+                {
+                    Console.Write("{0} ", arr1[i]);
+                }
+                Console.Write("\n\n");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ conditional-statements/Conditional_statements_t4/Conditional_statements_t4v2/Conditional_statements_t4v2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_statements_t4v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä kolme numeroa. Ohjelma järjestää numerot pienimmästä suurimpaan.");
+            Console.WriteLine("Anna ensimmäinen numero.");
+            String userInput1;
+            userInput1 = Console.ReadLine();
+
+            int a;
+            int.TryParse(userInput1, out a);
+
+            Console.WriteLine("Anna toinen numero");
+            String userInput2;
+            userInput2 = Console.ReadLine();
+
+            int b;
+            int.TryParse(userInput2, out b);
+
+            Console.WriteLine("Anna kolmas numero");
+            String userInput3;
+            userInput3 = Console.ReadLine();
+
+            int c;
+            int.TryParse(userInput3, out c);
+
+                if (a > b && b < c) ;
+                Console.WriteLine($"{b} {c} {a}");
+
+                else.if (a > b && c < b) ;
+                Console.WriteLine($"{c} {b} {a}");
+
+                else.if (b > a && c < a) ;
+                Console.WriteLine($"{b} {a} {c}");
+
+                else.if (b > a && a < c) ;
+                Console.WriteLine($"{b} {c} {a}");
+
+                else.if (c > b && b > a) ;
+                Console.WriteLine($"{c} {b} {a}");}
+
+                else.if (c > a && a > b) ;
+                Console.WriteLine($"{c} {a} {b}");
+            
+            Console.ReadKey();
+                
+        }
+    }
+}
