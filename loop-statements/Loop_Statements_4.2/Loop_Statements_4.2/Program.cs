1f5e8aa6e9081591772243687523c2ff9dd96b64
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-17 19:05:04 +0300

    4.2 loopit

---
+++ loop-statements/Loop_Statements_4.2/Loop_Statements_4.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Statements_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa lukujen 1-9 kertotaulut");
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            int n = 0;
+            do
+            {
+                int i = 0;
+                int f = 1;
+                n = n + 1;
+
+
+                do
+                {
+                    i = i + 1;
+                    f = i * n;
+
+                    Console.Write(n);
+                    Console.Write(" x ");
+                    Console.Write(i);
+                    Console.Write(" = ");
+                    Console.WriteLine(f);
+                }
+
+
+                while (i < 9);
+
+                Console.WriteLine("  ");
+            }
+
+            while (n < 9);
+            Console.ReadKey();
+
+        }
+    }
+}
