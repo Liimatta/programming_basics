dd5ba1eca854709a8bbd457013ab1d3777548602
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-17 18:50:32 +0300

    toistorakenne 4

---
+++ loop-statements/Loop_Statements_4.1/Loop_Statements_4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Statements_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa lukujen 1-10 neliöjuuret");
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            int i = 0;
+            int f = 1;
+
+            Console.WriteLine("Luku    Neliöjuuri");
+                do
+                {
+                    i = i + 1;
+                    f = i;
+                    Console.Write(i);
+                    Console.Write(" = ");
+                    Console.WriteLine(Math.Sqrt(f));
+                }
+
+                while (i < 10);
+
+            Console.ReadKey();
+
+        }
+    }
+}
