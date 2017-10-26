9b65017186a67d08e59d361d700357f41c60d8d3
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-26 17:06:23 +0300

    4.5

---
+++ loop-statements/Loop_statements_4.5/Loop_statements_4.5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_statements_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vakioveikkausrivi");
+
+
+            Random r = new Random();
+            int result = 0;
+
+
+
+            for (int i = 1; i < 14; i++)
+            {
+                result = r.Next(0, 10);
+                if (result <= 4)
+                {
+                    Console.Write(i);
+                    Console.WriteLine(") 1");
+                    
+                }
+                else if(result >= 6)
+                {
+                    Console.Write(i);
+                    Console.WriteLine(") 2");
+                }
+                else
+                {
+                    Console.Write(i);
+                    Console.WriteLine(") X");
+                }
+            }
+
+            Console.ReadKey();
+
+
+            //Janne L
+        }
+    }
+}
