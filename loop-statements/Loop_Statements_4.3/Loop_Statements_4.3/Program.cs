914acb7990d18623e3ff40558fc6da7f8ea12b2e
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-26 16:08:14 +0300

    boii

---
+++ loop-statements/Loop_Statements_4.3/Loop_Statements_4.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Statements_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Random r = new Random();
+
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista numeroa välillä 1-50");
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+
+            do
+
+            {
+                i = i + 1;
+
+                Console.Write(r.Next(50));
+                Console.Write(", ");
+                Console.Write(r.Next(50));
+                Console.Write(", ");
+                Console.Write(r.Next(50));
+                Console.Write(", ");
+                Console.Write(r.Next(50));
+                Console.Write(", ");
+                Console.Write(r.Next(50));
+                Console.WriteLine();
+            }
+
+            while (i < 4);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loop-statements/Loop_Statements_4.4/Loop_Statements_4.4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Statements_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's flip a coin 10000 times");
+
+            string userinput1;
+            userinput1 = Console.ReadLine();
+
+            Random r = new Random();
+            int klaava = 0;
+            int kruuna = 0;
+            int result = 0;
+            
+
+            
+            for (int i = 0; i< 5; i++)
+            {
+                result = Rand.Next(0, 2);
+                if (result == 1)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+            }
+            Console.WriteLine("heads was flipped {0} times",heads);
+            Console.WriteLine("tails was flipped {0} times", tails);
+            return result;
+
+
+
+        }
+        
+    }
+}
