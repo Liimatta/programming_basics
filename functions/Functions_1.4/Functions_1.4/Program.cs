f79de88ba72c7e6b751abdfdc0b27c718b464594
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-29 21:22:08 +0200

    asd

--- functions/Functions_1.4/Functions_1.4/Program.cs
+++ functions/Functions_1.4/Functions_1.4/Program.cs
@@ -10,6 +10,45 @@ namespace Functions_1._4
     {
         static void Main(string[] args)
         {
+
+            {
+                int count = 0;
+                int suurin = 0;
+                
+                while (count < 10)
+                {
+                    Console.WriteLine("Anna positiivinen kokonaisluku: ");
+
+                    string input;
+                    input = Console.ReadLine();
+                    int n = Convert.ToInt32(input);
+
+                    if (n >= 0)
+                    {
+                        count = n;
+                        count++;
+                    }
+            
+
+                    else
+
+                    {
+                        Console.WriteLine("Antamasi luku ei ole positiivinen kokonaisluku.");
+                    }
+
+                }
+
+            for (int x = 0; x < 10; x++)
+            {
+                    if (x > suurin) ;
+                
+            }
+
+            Console.WriteLine("Suurin luku oli {0}", suurin);
+
+
+            }
+
         }
     }
 }
