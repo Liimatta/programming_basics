51188095c2fa07f435e9810a8c41e54bb5083755
Janne Liimatta <janneliimatta@hotmail.com>
2017-10-29 15:38:43 +0200

    funktiot

---
+++ functions/Functions_1.2/Functions_1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä kaksi lukua");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            int c = string.Compare(a, b);
+
+            if (c == 1)
+
+                Console.WriteLine("{0} on pienempi kuin {1}",b,a);
+            else
+                Console.WriteLine("{0} on pienempi kuin {1}",a,b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ functions/Functions_1/Functions_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä numero");
+
+            string userinput1;
+            userinput1 = Console.ReadLine();
+            int n = Convert.ToInt32(userinput1);
+            if (n > 0)
+
+                Console.WriteLine(new String('*', n));
+            else
+                Console.WriteLine("luku {0} ei ole sallittu", n);
+            //Janne L
+            Console.ReadKey();
+        }
+    }
+}
